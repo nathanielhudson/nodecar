9190ef639260b561a8c35106e4824ca2314899df
Nathaniel Hudson <me@nathanielh.com>
2018-04-16 22:02:18 -0400

    test duty cycle functions

--- index.js
+++ index.js
@@ -34,30 +34,45 @@ http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
+function dutyCycle(pin, duty, tick){ //sloppy duty cycle algorithm. There's probably an actual algorithm for this but yolo
+  if (tick < duty) {
+    pin.write(1);
+  } else {
+    pin.write(0);
+  }
+};
+
+var tick = 0; //approx tenths of a second running. Duty cycle is per tenths of a second.
+var cps = 1/300 * 1000;
+
 setInterval(function(){
-  //TODO: FAKE PWM HERE.
-  if (drive > 0.2) {
-    m1d1.write(1);
+  tick+=cps/10;
+  if (tick > 1) {
+    tick--;
+  }
+
+  if (drive > 0.05) {
+    dutyCycle(m1d1, drive, tick);
     m1d2.write(0);
   } else if (drive < -0.2) {
     m1d1.write(0);
-    m1d2.write(1);
+    dutyCycle(m1d2, -drive, tick);
   } else {
     m1d1.write(0);
     m1d2.write(0);
   }
 
   if (turn > 0.2) {
-    m2d1.write(1);
+    dutyCycle(m2d1, turn, tick);
     m2d2.write(0);
   } else if (turn < -0.2) {
     m2d1.write(0);
-    m2d2.write(1);
+    dutyCycle(m2d2, -turn, tick);
   } else {
     m2d1.write(0);
     m2d2.write(0);
   }
-}, 1/10 * 1000);
+}, cps);
 
 function exit() {
   m1d1.unexport();
